38921d4a3030ffb52a6034055a3dad951d310122
eyzhang24 <89721157+eyzhang24@users.noreply.github.com>
2023-11-11 17:01:56 -0500

    Finish first draft of chapter 3

--- toy_examples/toy_code.R
+++ toy_examples/toy_code.R
@@ -18,12 +18,16 @@ Y <- exp(x/10) + 2*sin(x/2) + rnorm(50, mean = 0, sd = 0.5)
 df <- data.frame(x, Y)
 
 # plot data and linear regression line
-ggplot(df, aes(x, Y)) +
+q1 <- ggplot(df, aes(x, Y)) +
   geom_point() +
-  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), color = "darkorange") + 
+  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), 
+                linetype = "dashed", color = "darkorange") + 
   geom_smooth(method = "lm", formula = "y~x", 
-              color = "deepskyblue3", fill = "gray70", 
-              linetype = "dashed", linewidth = 0.5)
+              color = "deepskyblue3", fill = "gray70", linewidth = 0.5, se = F)
+
+# save plot
+ggsave("index/figures/ch3_toy1.png", plot = q1, device = "png", 
+       width = 5, height = 3)
 
 ########
 # kernel regression
@@ -35,48 +39,109 @@ df$Weight <- dnorm(df$x, mean = 12.5, sd = 1)
 # plot points colored by their weights
 p1 <- ggplot(df, aes(x, Y)) +
   geom_point(aes(color = Weight)) +
-  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), color = "darkorange") + 
+  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), 
+                linetype = "dashed", color = "darkorange") + 
   geom_vline(xintercept = 12.5, linetype = "dotted") +
-  theme(legend.position = c(0.95, 0.3))#c(0.1, 0.7))
+  theme(legend.position = "none")
+  # theme(legend.position = c(0.93, 0.29), 
+  #       legend.key.size = unit(0.35, 'cm'))#c(0.1, 0.7))
 
 # plot a curve of weights
 normcurv <- data.frame(x = seq(0, 25, length.out = 250)) 
-normcurv$Y <- dnorm(normcurv$x, mean = 12.5, sd = 1)
-p2 <- ggplot(normcurv, aes(x, Y, color = Y)) +
+normcurv$Weight <- dnorm(normcurv$x, mean = 12.5, sd = 1)
+p2 <- ggplot(normcurv, aes(x, Weight, color = Weight)) +
   geom_line() +
-  theme(legend.position = "none")
+  scale_y_continuous(breaks = c(0, 0.2, 0.4)) +
+  theme(legend.position = "none") 
 
 # stitch plots together
-cowplot::plot_grid(p1, p2, ncol = 1, rel_heights = c(0.7, 0.3))
+q2 <- cowplot::plot_grid(p1, p2, ncol = 1, rel_heights = c(0.7, 0.3))
+q2
 
-# fit kernel regression
+# save plot
+ggsave("index/figures/ch3_toy2.png", plot = q2, device = "png", 
+       width = 5, height = 4)
+
+# fit kernel regression with sigma = 1, bandwidth = 8/3
 kmr_toy <- ksmooth(df$x, df$Y, kernel = "normal", bandwidth = 8/3, x.points = df$x)
 df <- df |> 
   left_join(as.data.frame(kmr_toy), by = "x") |> 
   rename(Yhat = y)
 
 # plot kernel regression estimation
-ggplot(df) +
+q3 <- ggplot(df) +
   geom_point(aes(x, Y)) +
-  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), color = "darkorange") +
-  geom_line(aes(x, Yhat), color = "deepskyblue3", linetype = "dashed") 
+  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), 
+                linetype = "dashed", color = "darkorange") +
+  geom_line(aes(x, Yhat), color = "deepskyblue3") 
+
+# save plot
+ggsave("index/figures/ch3_toy3.png", plot = q3, device = "png", 
+       width = 5, height = 3)
+
+# fit kernel regression with sigma = 5, bandwidth = 40/3
+kmr_toy2 <- ksmooth(df$x, df$Y, kernel = "normal", bandwidth = 40/3, x.points = df$x)
+
+# fit kernel regression with sigma = 0.1, bandwith = 8/30
+kmr_toy3 <- ksmooth(df$x, df$Y, kernel = "normal", bandwidth = 8/30, x.points = df$x)
+
+# re-join data
+df2 <- df |> 
+  left_join(as.data.frame(kmr_toy2), by = "x") |> 
+  rename("rho = 50" = y) |> 
+  left_join(as.data.frame(kmr_toy3), by = "x") |> 
+  rename("rho = 0.02" = y) |> 
+  pivot_longer(cols = c("rho = 50", "rho = 0.02"), values_to = "Yhat")
+
+# plot kernel regression with two values of rho
+qrho <- ggplot(df2) +
+  geom_point(aes(x, Y)) +
+  geom_line(aes(x, Yhat), color = "deepskyblue3") +
+  facet_wrap(~name)
+
+# save plot
+ggsave("index/figures/ch3_toyrho.png", plot = qrho, device = "png", 
+       width = 7, height = 3)
 
 ########
 # spline regression
 ########
 
-# fit spline regression
 kn <- c(25/3, 50/3) # 3 knots of equal width
+
+# fit linear spline regression
+spline_toy_line <- lm(Y ~ bs(x, knots = kn, degree = 1), data = df)
+p_line <- predict(spline_toy_line, se = T)
+df$Yhats_line <- p_line$fit
+
+q4 <- ggplot(df) +
+  geom_point(aes(x, Y)) +
+  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), 
+                linetype = "dashed", color = "darkorange") +
+  geom_line(aes(x, Yhats_line), color = "deepskyblue3") +
+  # geom_ribbon(aes(x, ymin = Ylows_line, ymax = Yups_line), fill = "gray70", alpha = 0.3) +
+  geom_vline(xintercept = kn, linetype = "dotted")
+
+# save plot
+ggsave("index/figures/ch3_toy4.png", plot = q4, device = "png", 
+       width = 5, height = 3)
+
+# fit cubic/natural spline regression
 spline_toy <- lm(Y ~ bs(x, knots = kn), data = df)
 p <- predict(spline_toy, se = T)
 df$Yhats <- p$fit
-df$Ylows <- df$Yhats - 1.96 * p$se.fit
-df$Yups <- df$Yhats + 1.96 * p$se.fit
+# df$Ylows <- df$Yhats - 1.96 * p$se.fit
+# df$Yups <- df$Yhats + 1.96 * p$se.fit
 
 # plot spline regression estimation
-ggplot(df) +
+q5 <- ggplot(df) +
   geom_point(aes(x, Y)) +
-  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), color = "darkorange") +
-  geom_line(aes(x, Yhats), color = "deepskyblue3", linetype = "dashed") +
-  geom_ribbon(aes(x, ymin = Ylows, ymax = Yups), fill = "gray70", alpha = 0.3) +
+  geom_function(fun = function(x) exp(x/10) + 2*sin(x/2), 
+                linetype = "dashed", color = "darkorange") +
+  geom_line(aes(x, Yhats), color = "deepskyblue3") +
+  # geom_ribbon(aes(x, ymin = Ylows, ymax = Yups), fill = "gray70", alpha = 0.3) +
   geom_vline(xintercept = kn, linetype = "dotted")
+
+# save plot
+ggsave("index/figures/ch3_toy5.png", plot = q5, device = "png", 
+       width = 5, height = 3)
