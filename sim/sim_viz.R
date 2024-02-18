1bda3f41c7abd39022f3a5ecd406184b8f9339fa
eyzhang24 <89721157+eyzhang24@users.noreply.github.com>
2024-02-18 10:59:01 -0500

    Add in code to appendix, scale predictors in simulation

--- sim/sim_viz.R
+++ sim/sim_viz.R
@@ -9,10 +9,42 @@ theme_update(
   strip.text = element_text(color = "gray30")
 )
 
-#############
-#predictors
-#############
+########
+# before simulation
+########
 
+# correlation between exposures
+target_first <- read_csv("madres_data/target_first.csv")
+
+cor_mat <- cor(target_first[, 5:14], method = "spearman")
+cor_mat[lower.tri(cor_mat)] <- NA
+melt_cor <- reshape2::melt(cor_mat) |> 
+  mutate(label = ifelse(value == 1, NA, round(value, 2))) |> 
+  na.omit()
+melt_cor |> 
+  ggplot(aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = label), size = 3.5) +
+  scale_fill_gradient2(
+    limit = c(-0.6, 0.6), breaks = c(-0.6, -0.3, 0, 0.3, 0.6),
+    low = "deepskyblue3", mid = "white", high = "darkorange", 
+    na.value = NA) +
+  coord_fixed() +
+  labs(x = NULL, y = NULL, fill = TeX(r"( Spearman's $\rho$ )")) +
+  theme(
+    panel.grid.major.x = element_line(color = "grey85",
+                                      linewidth = 0.25,
+                                      linetype = 2), 
+    panel.border = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.9, 0.1),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+ggsave("index/figures/ch4_corr.png", width = 5, height = 5)
+
+# log-transformed versus original
 comb_small <- read_csv("madres_data/base_data.csv")
 
 #log-transform target data
@@ -21,36 +53,147 @@ comb_log <- comb_small |>
   #factors back to numeric
   mutate(across(where(is.factor), as.numeric))
 
-#look at predictor data distributions
+univ1 <- comb_small |> 
+  select(10:19) |> 
+  pivot_longer(cols = 1:10) |> 
+  mutate(name = factor(name, levels = names(comb_small)[10:19])) |> 
+  ggplot(aes(x = value)) +
+  geom_density() +
+  facet_wrap(~name, scales = "free", nrow = 2)
+univ2 <- comb_log |> 
+  select(10:19) |> 
+  pivot_longer(cols = 1:10) |> 
+  mutate(name = factor(name, levels = names(comb_small)[10:19])) |> 
+  ggplot(aes(x = value)) +
+  geom_density() +
+  facet_wrap(~name, scales = "free", nrow = 2)
+
+cowplot::plot_grid(univ1, univ2, labels = "auto", nrow = 2)
+
+ggsave("index/figures/ch4_univlog.png", width = 7.5, height = 5)
+
+#look at correlation between race and chemicals
+name_order <- c("As", "Cd", "Co", "Hg", "Ni", "Tl", "Pb", "Mo", "Sb", "Sn", 
+                "age", "bmi")
+comb_log |> 
+  select(c(6, 8:19)) |> 
+  pivot_longer(cols = 2:13, names_to = "key", values_to = "value") |> 
+  mutate(key = factor(key, levels = name_order)) |> 
+  mutate(race = as.factor(race)) |> 
+  ggplot(aes(x = race, y = value, color = race)) +
+  geom_boxplot() +
+  scale_color_discrete(
+    name = "Race by ethnicity\nand birth place", 
+    labels = c("Non-Hisp. white", "Non-Hisp. black", "Non-Hisp other", 
+               "Hispanic born\nin US", "Hispanic born\noutside US")) + 
+  theme(legend.spacing.y = unit(0.25, 'cm')) +
+  guides(color = guide_legend(byrow = TRUE)) +
+  labs(x = "Race, coded") +
+  facet_wrap(~key, scales = "free_y")
+ggsave("index/figures/ch4_race_exp.png", width = 6, height = 4)
+
+########
+# after simulation
+########
+
+#read small size back in
 out1 <- read_rds("sim/sim_preds_sm.RDS")
 
 comb_sim1 <- bind_rows(out1)
 
 #density plots
+name_order <- c("As", "Cd", "Co", "Hg", "Ni", "Tl", "Pb", "Mo", "Sb", "Sn", 
+                "age", "bmi")
 comb_sim1 |> 
   mutate(sim = as.factor(sim)) |> 
   select(3:15) |> 
   pivot_longer(cols = 1:12) |>
-  mutate(name = factor(name, levels = names(comb_sim1)[3:14])) |> 
+  mutate(name = factor(name, levels = name_order)) |> 
   ggplot(aes(x = value, group = sim)) +
   geom_line(stat = "density", color = "grey10", alpha = 0.01) + 
   geom_density(
-    data = comb_log |> select(8:19) |> pivot_longer(cols = 1:12),
+    data = comb_log |> select(8:19) |> pivot_longer(cols = 1:12) |> 
+      mutate(name = factor(name, levels = name_order)),
     mapping = aes(x = value), 
     color = "deepskyblue", linewidth = 0.75, inherit.aes = FALSE
   ) +
   facet_wrap(~name, scales = "free")
+ggsave("index/figures/ch4_univ_sim.png", width = 6, height = 4)
 
-#ggpairs
-comb_sim1_p <- comb_sim1 |> 
-  filter(sim <= 200) |> 
-  select(3:14) |>
-  mutate(sim = 0.01) |> 
-  bind_rows(mutate(select(comb_log, 8:19), 
-                   sim = 1))
+#scatterplots???
 
-GGally::ggpairs(comb_sim1_p, aes(alpha = sim, color = sim), columns = 1:12, 
-                upper = 'blank', diag = 'blank')
+#look at average correlation values 
+one <- out1[1:10]
+
+cors <- out1 |> 
+  purrr::map_df(\(x) {
+    cor_mat <- cor(x[, 5:14], method = "spearman")
+    cor_mat[lower.tri(cor_mat)] <- NA
+    melt_cor <- reshape2::melt(cor_mat) |> 
+      mutate(value = ifelse(value == 1, NA, value)) |> 
+      na.omit() |> 
+      mutate(sim = x$sim[1])
+    return(melt_cor)
+  })
+
+newbreaks <- function(lims) {
+  range <- diff(lims)
+  return(c(lims[1] + range/5, mean(lims), lims[2] - range/5))
+}
+
+#distribution
+cors |> 
+  group_by(Var1, Var2) |> 
+  mutate(mean = mean(value)) |> 
+  ungroup() |> 
+  ggplot(aes(x = value, fill = mean)) +
+  geom_density() + 
+  scale_x_continuous(breaks = newbreaks, 
+                     labels = ~round(.x, 2)) +
+  scale_y_continuous(position = "right") +
+  scale_fill_gradient2(
+    limit = c(-0.6, 0.6), breaks = c(-0.6, -0.3, 0, 0.3, 0.6),
+    low = "deepskyblue3", mid = "white", high = "darkorange", 
+    na.value = NA) +
+  ggh4x::facet_grid2(fct_rev(Var2) ~ Var1, scales = "free", independent = "x", 
+                     render_empty = FALSE, switch = "both") +
+  labs(x = TeX(r"( Spearman's $\rho$ )")) +
+  theme(strip.placement = "outside", 
+        strip.text.y.left = element_text(angle = 0), 
+        legend.justification = c(1, 0),
+        legend.position = c(0.9, 0.1),
+        legend.direction = "horizontal") +
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+ggsave("index/figures/ch4_corr_sim.png", width = 10, height = 7)
+
+#average value
+cors |> 
+  group_by(Var1, Var2) |> 
+  summarize(value = mean(value)) |> 
+  mutate(label = round(value, 2)) |> 
+  ggplot(aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = label), size = 3.5) +
+  scale_fill_gradient2(
+    limit = c(-0.6, 0.6), breaks = c(-0.6, -0.3, 0, 0.3, 0.6),
+    low = "deepskyblue3", mid = "white", high = "darkorange", 
+    na.value = NA) +
+  coord_fixed() +
+  labs(x = NULL, y = NULL, fill = TeX(r"( Spearman's $\rho$ )")) +
+  theme(
+    panel.grid.major.x = element_line(color = "grey85",
+                                      linewidth = 0.25,
+                                      linetype = 2), 
+    panel.border = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.9, 0.1),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+ggsave("index/figures/ch4_corr_avg_sim.png", width = 5, height = 5)
 
 #############
 #match label with equation
