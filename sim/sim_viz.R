3cba984185a5f9c9e32d5d66b6fdab4410be7cd1
eyzhang24 <89721157+eyzhang24@users.noreply.github.com>
2024-02-24 20:20:07 -0500

    Finish draft of methods for simulating data

--- sim/sim_viz.R
+++ sim/sim_viz.R
@@ -10,7 +10,7 @@ theme_update(
 )
 
 ########
-# before simulation
+# observed data
 ########
 
 # correlation between exposures
@@ -134,8 +134,9 @@ comb_log |>
   facet_wrap(~key, scales = "free_y")
 ggsave("index/figures/ch4_race_exp.png", width = 6, height = 4)
 
+
 ########
-# after simulation
+# simulated data small
 ########
 
 #read small size back in
@@ -295,6 +296,167 @@ ggsave("index/figures/ch4_corr_sim+orig.png", width = 10, height = 5)
 cowplot::plot_grid(top_row, cors_dens, labels = c("", "c"), label_size = 16, 
                    nrow = 2, rel_heights = c(5, 7), scale = c(1, 0.95))
 ggsave("index/figures/ch4_corr_simorigdens.png", width = 10, height = 12)
+
+#############
+# simulated data large
+#############
+#read small size back in
+out2 <- read_rds("sim/sim_preds_lg.RDS")
+
+comb_sim2 <- bind_rows(out2)
+
+one <- comb_sim2[1:2000, ]
+
+#density plots for exposures
+name_order <- c("As", "Cd", "Co", "Hg", "Ni", "Tl", "Pb", "Mo", "Sb", "Sn")
+# "age", "bmi")
+comb_sim2 |> 
+  mutate(sim = as.factor(sim)) |> 
+  select(5:15) |> 
+  pivot_longer(cols = 1:10) |>
+  mutate(name = factor(name, levels = name_order)) |> 
+  ggplot(aes(x = value, group = sim)) +
+  geom_line(stat = "density", color = "grey10", alpha = 0.01) + 
+  geom_density(
+    data = comb_log |> select(10:19) |> pivot_longer(cols = 1:10) |> 
+      mutate(name = factor(name, levels = name_order)),
+    mapping = aes(x = value), 
+    color = "deepskyblue", linewidth = 0.75, inherit.aes = FALSE
+  ) +
+  facet_wrap(~name, scales = "free")
+ggsave("index/figures/ch4_univ_exp_sim_lg.png", width = 6, height = 4)
+
+#density plot for covariates
+cov_sim_p2 <- comb_sim2 |> 
+  mutate(sim = as.factor(sim)) |> 
+  select(age, bmi, sim) |> 
+  pivot_longer(cols = 1:2) |>
+  ggplot(aes(x = value, group = sim)) +
+  geom_line(stat = "density", color = "grey10", alpha = 0.01) + 
+  geom_density(
+    data = comb_log |> select(age, bmi) |> pivot_longer(cols = 1:2),
+    mapping = aes(x = value), 
+    color = "deepskyblue", linewidth = 0.75, inherit.aes = FALSE
+  ) +
+  facet_wrap(~name, scales = "free", ncol = 1)
+#bar plot + violin plot for covariates
+cov_sim_q2 <- comb_sim2 |> 
+  mutate(sim = as.factor(sim)) |> 
+  select(sim, smoke, race) |> 
+  mutate(smoke = ifelse(smoke == 0, "Never-exposed", "Ever-exposed"), 
+         race = case_when(
+           race == 1 ~ "Non-Hisp. white", 
+           race == 2 ~ "Non-Hisp. black", 
+           race == 3 ~ "Non-Hisp. other", 
+           race == 4 ~ "Hispanic born\nin US", 
+           race == 5 ~ "Hispanic born\noutside US"
+         )) |> 
+  pivot_longer(cols = 2:3) |> 
+  group_by(sim, name, value) |> 
+  summarize(prop = n()/1000) |> 
+  mutate(value = factor(
+    value, levels = rev(c("Never-exposed", "Ever-exposed",
+                          "Non-Hisp. white", "Non-Hisp. black", "Non-Hisp. other", 
+                          "Hispanic born\nin US", "Hispanic born\noutside US"))
+  )) |> 
+  ggplot(aes(x = value, y = prop)) +
+  geom_bar(data = df_forcovcat, aes(x = value, y = after_stat(prop), group = 1), 
+           inherit.aes = FALSE, stat = "count", fill = "skyblue") +
+  geom_violin(color = "gray30", fill = "gray", alpha = 0.25) + 
+  facet_wrap(~name, scales = "free", ncol = 1) +
+  coord_flip() +
+  labs(x = NULL, y = "proportion")
+
+cov_sim_q2
+
+cowplot::plot_grid(cov_sim_p2, cov_sim_q2, labels = "auto", nrow = 1, 
+                   rel_widths = c(0.4, 0.6))
+
+ggsave("index/figures/ch4_univ_cov_sim_lg.png", width = 6, height = 4)
+
+#correlation plots, large
+corl <- out2 |> 
+  purrr::map_df(\(x) {
+    cor_mat <- cor(x[, 5:14], method = "spearman")
+    cor_mat[lower.tri(cor_mat)] <- NA
+    melt_cor <- reshape2::melt(cor_mat) |> 
+      mutate(value = ifelse(value == 1, NA, value)) |> 
+      na.omit() |> 
+      mutate(sim = x$sim[1])
+    return(melt_cor)
+  })
+
+newbreaks <- function(lims) {
+  range <- diff(lims)
+  return(c(lims[1] + range/5, mean(lims), lims[2] - range/5))
+}
+#distribution
+cors_dens2 <- corl |> 
+  group_by(Var1, Var2) |> 
+  mutate(mean = mean(value)) |> 
+  ungroup() |> 
+  ggplot(aes(x = value, fill = mean)) +
+  geom_density() + 
+  scale_x_continuous(breaks = newbreaks, 
+                     labels = ~round(.x, 2)) +
+  scale_y_continuous(position = "right") +
+  scale_fill_gradient2(
+    limit = c(-0.6, 0.6), breaks = c(-0.6, -0.3, 0, 0.3, 0.6),
+    low = "deepskyblue3", mid = "white", high = "darkorange", 
+    na.value = NA) +
+  ggh4x::facet_grid2(fct_rev(Var2) ~ Var1, scales = "free", independent = "x", 
+                     render_empty = FALSE, switch = "both") +
+  labs(x = TeX(r"( Spearman's $\rho$ )")) +
+  theme(strip.placement = "outside", 
+        strip.text.y.left = element_text(angle = 0), 
+        legend.justification = c(1, 0),
+        legend.position = c(0.9, 0.1),
+        legend.direction = "horizontal") +
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+cors_dens2
+
+# ggsave("index/figures/ch4_corr_sim.png", width = 10, height = 7)
+
+#average value
+cor_sim2 <- corl |> 
+  group_by(Var1, Var2) |> 
+  summarize(value = mean(value)) |> 
+  mutate(label = round(value, 2)) |> 
+  ggplot(aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = label), size = 3.5) +
+  scale_fill_gradient2(
+    limit = c(-0.6, 0.6), breaks = c(-0.6, -0.3, 0, 0.3, 0.6),
+    low = "deepskyblue3", mid = "white", high = "darkorange", 
+    na.value = NA) +
+  coord_fixed() +
+  labs(x = NULL, y = NULL, fill = TeX(r"( Spearman's $\rho$ )")) +
+  theme(
+    panel.grid.major.x = element_line(color = "grey85",
+                                      linewidth = 0.25,
+                                      linetype = 2), 
+    panel.border = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.9, 0.1),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+cor_sim2
+
+# ggsave("index/figures/ch4_corr_avg_sim.png", width = 5, height = 5)
+
+#put original and simulated correlation together
+top_row2 <- cowplot::plot_grid(cor_orig, cor_sim2, labels = "auto", label_size = 16, 
+                              nrow = 1, scale = 0.95)
+top_row2
+# ggsave("index/figures/ch4_corr_sim+orig.png", width = 10, height = 5)
+
+#put original, simulated, and density correlation together
+cowplot::plot_grid(top_row2, cors_dens2, labels = c("", "c"), label_size = 16, 
+                   nrow = 2, rel_heights = c(5, 7), scale = c(1, 0.95))
+ggsave("index/figures/ch4_corr_lg_simorigdens.png", width = 10, height = 12)
+
 
 #############
 #match label with equation
