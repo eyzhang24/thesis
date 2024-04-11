4265fd19b57a240475a645f5e7ab21e4f4a4c1c6
eyzhang24 <89721157+eyzhang24@users.noreply.github.com>
2024-04-11 08:16:25 -0400

    Add exposure response output for BSR stratified

---
+++ results_re.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(latex2exp)
+
+# set up ------------------------------------------------------------------
+
+# set theme for plots
+theme_set(theme_light())
+theme_update(
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  strip.background = element_rect(color="gray", fill="white"), 
+  strip.text = element_text(color = "gray30")
+)
+
+# naive -------------------------------------------------------------------
+
+
+
+
+# oracle ------------------------------------------------------------------
+
+
+
+
+# bkmr --------------------------------------------------------------------
+
+
+
+
+# bsr ---------------------------------------------------------------------
+
+
