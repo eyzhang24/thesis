69f03c3a50168b6ca0fd19d574881cc64afa51f9
eyzhang24 <89721157+eyzhang24@users.noreply.github.com>
2024-04-17 10:43:15 -0400

    Make some figures for presentation

--- misc_code/plot_3d_surf.R
+++ misc_code/plot_3d_surf.R
@@ -173,5 +173,24 @@ figcp2
 save_image(figcp2, "index/figures/surfaces/cp2.png", 
            width = 720, height = 480, scale = 3)
 
+##########
+# for presentation
+##########
+
+# no interaction
+y98 <- with(data, x1 + x2) 
+fig98 <- create_plot(x1, x2, y98, "x1", "x2")  |> 
+  layout(scene = list(camera = list(eye = list(x = -1, y = 2, z = .1))))
+fig98
+save_image(fig98, "misc_code/p98.png", 
+           width = 720, height = 480, scale = 3)
+
+# multiplicative interaction, smaller effect size
+y99 <- with(data, x1 + x2 + 0.5*x1*x2) 
+fig99 <- create_plot(x1, x2, y99, "x1", "x2") |> 
+  layout(scene = list(camera = list(eye = list(x = -1, y = 2, z = 0.1))))
+fig99
+save_image(fig99, "misc_code/p99.png", 
+           width = 720, height = 480, scale = 3)
 
 
